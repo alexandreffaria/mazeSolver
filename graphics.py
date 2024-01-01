b116b60a16697101522f931a03ee4d60bb5ca07b
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-01 19:39:55 -0300

    Not inheriting tk anymore

--- graphics.py
+++ graphics.py
@@ -1,25 +1,27 @@
 from tkinter import Tk, BOTH, Canvas
 
-class Window(Tk):
+class Window():
     def __init__(self, width, height):
         super().__init__()
         self.width = width
         self.height = height
-        self.title("Maze Solver | Boot.dev | @Alexandreffaria")
+        self.root = Tk()
+        self.root.title("Maze Solver | Boot.dev | @Alexandreffaria")
         self.canvas = Canvas(height=self.height, width=self.width)
         self.canvas.pack()
         self.isRunning = False
-        self.protocol("WM_DELETE_WINDOW", self.close)
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.update_idletasks()
-        self.update()
+        self.root.update_idletasks()
+        self.root.update()
 
     def wait_for_close(self):
         self.isRunning = True
         while self.isRunning:
             self.redraw()
-    
+        print("window closed...")
+
     def close(self):
         self.isRunning = False
     
