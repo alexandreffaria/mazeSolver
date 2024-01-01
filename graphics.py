e3b58f1028c2683f2186ebb9eabcc1d8da59cb24
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-01 20:13:30 -0300

    Point and line classes and draw_line method

--- graphics.py
+++ graphics.py
@@ -9,6 +9,9 @@ class Window():
         self.isRunning = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
     def redraw(self):
         self.root.update_idletasks()
         self.root.update()
@@ -24,4 +27,17 @@ class Window():
 
 
 class Point():
-    def __init__(self, x, y):
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+class Line():
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
+        )
+        canvas.pack()
