11028a8b581afa6d34d9dff6cfa3079ec9b1df61
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-01 20:16:59 -0300

    sugar

--- mazeSolver.py
+++ mazeSolver.py
@@ -1,16 +1,16 @@
 from graphics import Window, Point, Line
 
 def main():
-    janela = Window(
-    800, 600
-    )
+    janela = Window(800, 600)
 
     ponto1 = Point()
     ponto1.x = 10
     ponto1.y = 10
+
     ponto2 = Point()
     ponto2.x = 600
     ponto2.y = 100
+    
     linha = Line(ponto1,ponto2)
     janela.draw_line(linha, "salmon")
 
