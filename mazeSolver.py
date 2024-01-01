1bcc461185ea4f3a580c342e6f4b31bc4930468e
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-01 20:19:00 -0300

    passing x and y on init now

--- mazeSolver.py
+++ mazeSolver.py
@@ -3,14 +3,10 @@ from graphics import Window, Point, Line
 def main():
     janela = Window(800, 600)
 
-    ponto1 = Point()
-    ponto1.x = 10
-    ponto1.y = 10
+    ponto1 = Point(10,10)
 
-    ponto2 = Point()
-    ponto2.x = 600
-    ponto2.y = 100
-    
+    ponto2 = Point(600,100)
+
     linha = Line(ponto1,ponto2)
     janela.draw_line(linha, "salmon")
 
