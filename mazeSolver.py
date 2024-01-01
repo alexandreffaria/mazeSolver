192022b50d50f4c8a4f6b017bae2f44e8c724828
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-01 20:13:45 -0300

    testing self.draw_line()

--- mazeSolver.py
+++ mazeSolver.py
@@ -1,9 +1,19 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     janela = Window(
     800, 600
     )
+
+    ponto1 = Point()
+    ponto1.x = 10
+    ponto1.y = 10
+    ponto2 = Point()
+    ponto2.x = 600
+    ponto2.y = 100
+    linha = Line(ponto1,ponto2)
+    janela.draw_line(linha, "salmon")
+
     janela.wait_for_close()
 
 if __name__ == "__main__":
