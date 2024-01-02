6b093c0a54071f248c1c3981b6fb7288ec471e78
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-01 23:15:51 -0300

    testing cell class

--- mazeSolver.py
+++ mazeSolver.py
@@ -1,4 +1,4 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 def main():
     janela = Window(800, 600)
@@ -9,6 +9,20 @@ def main():
 
     linha = Line(ponto1,ponto2)
     janela.draw_line(linha, "salmon")
+
+    celula = Cell(ponto1, ponto2, janela)
+    celula.draw()
+
+    celula2 = Cell(Point(400,300), Point(500,400), janela)
+    celula2.has_bottom_wall = False
+    celula2.draw()
+
+    celula3 = Cell(Point(125,125), Point(700,700), janela)
+    celula3.has_bottom_wall = False
+    celula3.has_left_wall = False
+    celula3.draw()
+
+
 
     janela.wait_for_close()
 
