b46ff510c0096b93693f6a7d76d13c030732c9c9
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-01 23:23:29 -0300

    sugar

--- mazeSolver.py
+++ mazeSolver.py
@@ -10,20 +10,18 @@ def main():
     linha = Line(ponto1,ponto2)
     janela.draw_line(linha, "salmon")
 
-    celula = Cell( janela)
+    celula = Cell(janela)
     celula.draw(10,10,249,100)
 
-    celula2 = Cell( janela)
+    celula2 = Cell(janela)
     celula2.has_bottom_wall = False
     celula2.draw(250,250, 300, 300)
 
-    celula3 = Cell( janela)
+    celula3 = Cell(janela)
     celula3.has_bottom_wall = False
     celula3.has_left_wall = False
     celula3.draw(123, 203, 432, 39)
-
-
-
+    
     janela.wait_for_close()
 
 if __name__ == "__main__":
