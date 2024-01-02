9526302f9e0fcc7ca244aec965937363b5afc97e
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-02 00:07:20 -0300

    draw_move

--- cell.py
+++ cell.py
@@ -1,3 +1,6 @@
+from graphics import Line, Point
+
+
 class Cell():
     def __init__(self, window):
         self.has_left_wall = True
@@ -28,3 +31,17 @@ class Cell():
         if self.has_bottom_wall:
             line = Line(Point(x1,y2),Point(x2,y2))
             self.__win.draw_line(line)
+    
+    def draw_move(self, to_cell, undo=False):
+        if not undo:
+            color_line = "cyan"
+        else:
+            color_line = "gray"
+        
+        center_self = Point((self.__x1 + self.__x2)/2, (self.__y1 + self.__y2)/2)
+        center_to_cell = Point((to_cell.__x1 + to_cell.__x2)/2, (to_cell.__y1 + to_cell.__y2)/2)
+
+        line = Line(center_self, center_to_cell)
+        self.__win.draw_line(line, fill_color=color_line)
+
+        
