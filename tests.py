4e4b6167fd75215410a9115a8aa5423dd82119ea
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-03 00:39:53 -0300

    testing break entrance and exit

--- tests.py
+++ tests.py
@@ -28,5 +28,15 @@ class Test(unittest.TestCase):
             num_rows
         )
 
+    def test_maze_break_entrance_and_exit(self):
+        maze = Maze(0, 0, 3, 3, 10, 10)
+        maze._break_entrance_and_exit()
+
+        entrance_cell = maze._cells[0][0]
+        self.assertFalse(entrance_cell.has_top_wall)
+
+        exit_cell = maze._cells[maze._num_cols - 1][maze._num_rows - 1]
+        self.assertFalse(exit_cell.has_bottom_wall)
+
 if __name__ == "__main__":
     unittest.main()
