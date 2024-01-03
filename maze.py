7d16f62dea2bbdfc1f6397efbc4199dcb9f84527
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-02 23:12:39 -0300

    renaming variables and implementing instructors code, it was working i promise, i like this better.

--- maze.py
+++ maze.py
@@ -12,31 +12,41 @@ class Maze():
         cell_size_y,
         win,
         ):
-        self.x1 = x1
-        self.y1 = y1
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.cell_size_x = cell_size_x
-        self.cell_size_y = cell_size_y
-        self.__win = win
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
         self._create_cells()
 
     def _create_cells(self):
-        self._cells = []
-        for i in range(self.num_cols):
-            for j in range(self.num_rows):
-                self._cells.append(Cell(self.__win))
-                self._draw_cell(i, j)
+        
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i,j)
 
     def _draw_cell(self, i, j):
-        cell = self._cells[i * self.num_rows + j]
-        x1 = self.x1 + i * self.cell_size_x
-        y1 = self.y1 + j * self.cell_size_y
-        x2 = x1 + self.cell_size_x
-        y2 = y1 + self.cell_size_y
-        cell.draw(x1, y1, x2, y2)
+        if not self._win:
+            return
+
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
         
     def _animate(self):
-        self.__win.redraw()
-        sleep(.1)
+        if not self._win:
+            return
+        
+        self._win.redraw()
+        sleep(.005)
