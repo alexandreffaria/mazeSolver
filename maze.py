619c9cc602a3d5385c48ab5a594d97059d78906c
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-06 17:58:09 -0300

    Adding random seed. _break_walls_r and knock_down_wall

--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
 from cell import Cell
 from time import sleep
+import random
 
 class Maze():
     def __init__(
@@ -10,7 +11,8 @@ class Maze():
         num_cols,
         cell_size_x,
         cell_size_y,
-        win=None,
+        win = None,
+        seed = None
         ):
         self._cells = []
         self._x1 = x1
@@ -21,6 +23,12 @@ class Maze():
         self._cell_size_y = cell_size_y
         self._win = win
         self._create_cells()
+        
+        if seed is not None:
+            self.seed = random.seed(seed)
+        else:
+            self.seed = seed
+
 
     def _create_cells(self):
         
@@ -57,3 +65,50 @@ class Maze():
         self._draw_cell(0,0)
         self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+
+    def _break_walls_r(self, i, j):
+        # mark the current cell at (i, j) as visited
+        self._cells[i][j].visited = True
+
+        # create a list of possible movements (in our case, to the top, right, bottom, and left cells)
+        directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
+        # shuffle the directions to ensure a random path each time
+        random.shuffle(directions)
+
+        # check each direction
+        for dx, dy in directions:
+            new_i, new_j = i + dx, j + dy
+
+            # checks if the new cell is valid (within the grid and not visited yet)
+            if (0 <= new_i < len(self._cells)) and (0 <= new_j < len(self._cells[i])) and not self._cells[new_i][new_j].visited:
+                # knock down the wall to the new cell
+                self._knock_down_wall(i, j, (dx, dy))
+
+                # recursively call the function for the new cell
+                self._break_walls_r(new_i, new_j)
+
+    def _knock_down_wall(self, i, j, direction):
+        """
+        Knock down wall between current cell and adjacent one in given direction.
+        """
+        dx, dy = direction
+        cell = self._cells[i][j]
+        other_cell = self._cells[i + dx][j + dy]
+
+        if dx == -1:   # moving left
+            cell.has_left_wall = False
+            other_cell.has_right_wall = False
+        elif dx == 1:  # moving right
+            cell.has_right_wall = False
+            other_cell.has_left_wall = False
+        elif dy == -1: # moving up
+            cell.has_top_wall = False
+            other_cell.has_bottom_wall = False
+        elif dy == 1:  # moving down
+            cell.has_bottom_wall = False
+            other_cell.has_top_wall = False
+
+        # After knocking down the walls, redraw the cells
+        self._draw_cell(i, j)
+        self._draw_cell(i + dx, j + dy)
